0d11331dd8e267d80fb88437dfaf28ddd1f1352f
Ricardo Russon <ricardo@buzzcloud.com.au>
2015-05-06 08:38:52 +1000

    Create wpsearch.sh
    initial commit.

---
+++ wpsearch.sh
@@ -0,0 +1,16 @@
+grep -lr --include=.*.php '' /var/www/vhosts/ ;
+grep -lr --include=*.php "=\"stop_\"" /var/www/vhosts/ ;
+grep -lr --include=*.php " = Array('1'=>'" /var/www/vhosts/ ;
+grep -lr --include=revslider3.zip '' /var/www/vhosts/ ;
+grep -lr --include=*.php ';}?><?php' /var/www/vhosts/ ;
+grep -lr --include=*.php '\]; $GLOBALS\[' /var/www/vhosts/ ;
+grep -lr --include=*.php 'eval(base64_decode(' /var/www/vhosts/ ;
+grep -lr --include=*.php 'eval(gzinflate(base64_decode' /var/www/vhosts/ ;
+grep -lr --include=*.php ' eval' /var/www/vhosts/*/httpdocs/wp-content/uploads/ ;
+grep -lr --include=*.php 'return base64_decode(' /var/www/vhosts/*/httpdocs/wp-content/uploads/ ;
+grep -lr --include=*.php 'eval(' /var/www/vhosts/*/httpdocs/wp-content/uploads/ ;
+grep -lr --include=*.php 'eval (' /var/www/vhosts/*/httpdocs/wp-content/uploads/ ;
+grep -lr --include=*.php '<form method="POST" action="" enctype="multipart/form-data"><input type="file" name="image"><input type="Submit" name="Submit" value="Submit"></form>' /var/www/vhosts/ ;
+# grep -lr --include=*.php '' /var/www/vhosts/*/httpdocs/wp-content/uploads/ ;
+# find /var/www/vhosts/ -type d -perm 777 -exec find {} -name "*.php" \;
+/usr/sbin/postqueue -p;

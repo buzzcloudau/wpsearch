24d580b953d72fe907ebe69ef6060a8fac79d438
Ricardo Russon <ricardo@buzzcloud.com.au>
2015-07-06 22:50:10 +1000

    Update wpsearch.sh
    Updated tests and added comments

--- wpsearch.sh
+++ wpsearch.sh
@@ -1,3 +1,5 @@
+## Search for Known WP Infection Signatures
+
 grep -lr --include=.*.php '' /var/www/vhosts/ ;
 grep -lr --include=*.php "=\"stop_\"" /var/www/vhosts/ ;
 grep -lr --include=*.php " = Array('1'=>'" /var/www/vhosts/ ;
@@ -11,6 +13,18 @@ grep -lr --include=*.php 'return base64_decode(' /var/www/vhosts/*/httpdocs/wp-c
 grep -lr --include=*.php 'eval(' /var/www/vhosts/*/httpdocs/wp-content/uploads/ ;
 grep -lr --include=*.php 'eval (' /var/www/vhosts/*/httpdocs/wp-content/uploads/ ;
 grep -lr --include=*.php '<form method="POST" action="" enctype="multipart/form-data"><input type="file" name="image"><input type="Submit" name="Submit" value="Submit"></form>' /var/www/vhosts/ ;
+grep -lr --include=*.suspected '' /var/www/vhosts/ ;
+grep -lr --include=*.php "strtolower($sF" /var/www/vhosts/ ;
+
+###
+# WARNING : Only uncomment these lines to deep search. 
+# May give false positives
+###
+
+# egrep -Rl --include=*.php --exclude=*data.inc.php '\$GLOBALS.*\\x' /var/www/vhosts/
 # grep -lr --include=*.php '' /var/www/vhosts/*/httpdocs/wp-content/uploads/ ;
 # find /var/www/vhosts/ -type d -perm 777 -exec find {} -name "*.php" \;
+
+## Check if anything is in the queue
+
 /usr/sbin/postqueue -p;
